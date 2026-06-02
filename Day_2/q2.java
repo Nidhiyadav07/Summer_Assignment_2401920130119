183837d76003e31644f25801647048e22bcf1a8f
Nidhiyadav07 <yadavnidhi9034@gmail.com>
2026-06-02 17:04:49 +0530

    Add files via upload

---
+++ Day_2/q2.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+
+public class q2 {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 1, 2, 2};
+
+        q2 obj = new q2();
+        System.out.println(obj.containsDuplicate(nums));
+    }
+}
