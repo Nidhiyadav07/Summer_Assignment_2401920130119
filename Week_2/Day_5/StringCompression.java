b68452a3996c0ed5b0480b7921e4153b8e6660c8
Nidhiyadav07 <yadavnidhi9034@gmail.com>
2026-06-12 19:30:31 +0530

    Add files via upload

---
+++ Week_2/Day_5/StringCompression.java
@@ -0,0 +1,41 @@
+public class StringCompression {
+
+    public static int compress(char[] chars) {
+        int index = 0;
+        int i = 0;
+
+        while (i < chars.length) {
+            char current = chars[i];
+            int count = 0;
+
+            while (i < chars.length && chars[i] == current) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = current;
+
+            if (count > 1) {
+                String num = String.valueOf(count);
+                for (char c : num.toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'a','a','b','b','c','c','c'};
+
+        int length = compress(chars);
+
+        System.out.println("Compressed String:");
+        for (int i = 0; i < length; i++) {
+            System.out.print(chars[i]);
+        }
+
+        System.out.println("\nLength = " + length);
+    }
+}
